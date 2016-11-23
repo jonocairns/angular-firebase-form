5257afe270ac05fc6f1541fe1304a44dade36b5f
Jonathan Cairns <jonathan.cairns@intergen.co.nz>
2016-11-24 10:42:07 +1300

    adding logo

--- app/app.js
+++ app/app.js
@@ -16,19 +16,80 @@ angular.module('myApp', [
   'ngAnimate',
   'ngFileUpload'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  //$locationProvider.hashPrefix('!');
-   var config = {
-    apiKey: "AIzaSyCIs9ZBO2T0k7nBv0qRkKq2-lOXowjWKws",
-    authDomain: "cz-training-agre-1479345541933.firebaseapp.com",
-    databaseURL: "https://cz-training-agre-1479345541933.firebaseio.com",
-    storageBucket: "cz-training-agre-1479345541933.appspot.com",
-    messagingSenderId: "128605872066"
-  };
-  firebase.initializeApp(config);
-  $routeProvider.otherwise({redirectTo: '/employer-details'});
-}]).controller('AppCtrl', AppCtrl);
+  config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    //$locationProvider.hashPrefix('!');
+    var config = {
+      apiKey: "AIzaSyCIs9ZBO2T0k7nBv0qRkKq2-lOXowjWKws",
+      authDomain: "cz-training-agre-1479345541933.firebaseapp.com",
+      databaseURL: "https://cz-training-agre-1479345541933.firebaseio.com",
+      storageBucket: "cz-training-agre-1479345541933.appspot.com",
+      messagingSenderId: "128605872066"
+    };
+    firebase.initializeApp(config);
+    $routeProvider.otherwise({ redirectTo: '/employer-details' });
+  }]).controller('AppCtrl', AppCtrl).controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log, $firebaseObject, $firebaseArray) {
+    $scope.close = function () {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav('right').close()
+        .then(function () {
+        });
 
-function AppCtrl($scope) {
-    $scope.currentNavItem = 'page1';
+
+    };
+
+    $scope.determinateValue = 78;
+
+
+    var ref = firebase.database().ref().child("messages").orderByChild("timestamp").limitToLast(7);
+    var list = $firebaseArray(ref);
+
+    $scope.messages = list;
+
+    $scope.send = function () {
+
+      list.$add({ text: $scope.message, timestamp: Date.now(), user: 'Terry' });
+      $scope.message = '';
+    }
+  });
+
+
+;
+
+function AppCtrl($scope, $mdSidenav, $firebaseObject) {
+  $scope.currentNavItem = 'page1';
+
+  var lol = firebase.database().ref().child("data");
+  var obj = $firebaseObject(lol);
+
+  var compute = function() {
+    var keys = Object.keys(obj);
+    var cleanKeys = [];
+    keys.forEach(function(key) {
+      if(key.indexOf('$') === -1) {
+        cleanKeys.push(key);
+      }
+    });
+    
+    var c = cleanKeys.length;
+    var completed = 0;
+    cleanKeys.forEach(function (v) {
+      
+      if (obj[v] === '' || typeof obj[v] === 'undefined' || obj[v] === null) {
+
+      } else {
+        completed++;
+      }
+
+    });
+    $scope.determinateValue = Math.ceil((completed / c) * 100);
   }
+
+  obj.$loaded().then(compute);
+
+  obj.$watch(compute);
+
+  $scope.toggleRight = function () {
+    $mdSidenav('right')
+      .toggle()
+  }
+}
--- app/view6/view2.js
+++ app/view6/view2.js
@@ -2,13 +2,14 @@
 
 angular.module('myApp.view6', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/checklist', {
-    templateUrl: 'view6/view2.html',
-    controller: 'View6Ctrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/checklist', {
+      templateUrl: 'view6/view2.html',
+      controller: 'View6Ctrl'
+    });
+  }])
 
-.controller('View6Ctrl', [function() {
+  .controller('View6Ctrl', ["$scope", "$firebaseObject", "$timeout",
+    function ($scope, $firebaseObject, $timeout) {
 
-}]);
+    }]);  
